598f612381ff81f6cc2046d4027c13835b4324bc
nlev <nick@levesque.io>
2025-03-03 22:26:44 -0500

    improvements

--- custom.el
+++ custom.el
@@ -4,57 +4,30 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098"
-     "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19"
-     "6f1f6a1a3cff62cc860ad6e787151b9b8599f4471d40ed746ea2819fcd184e1a"
-     "9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a"
-     "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14"
-     "691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b"
-     "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e"
-     "3d3eef7dd80e89aee44be1ff51d8285045615ea3239358a51818f4deaa5bd558"
-     "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
-     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
-     "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
-     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
+   '("b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19"
+     "c8c4baac2988652a760554e0e7ce11a0fe0f8468736be2b79355c9d9cc14b751"
+     "f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66"
      default))
  '(package-selected-packages
-   '(ace-window all-the-icons avy company consult consult-eglot
-		consult-project consult-project-extra diminish
-		doom-modeline doom-themes dtrt-indent eldoc-box embark
-		embark-consult evil-collection exec-path-from-shell
-		eyebrowse flycheck general git-gutter
-		graphviz-dot-mode magit marginalia obsidian orderless
-		smooth-scrolling spacious-padding treemacs
-		treesit-auto vertico vterm zig-mode))
- '(safe-local-variable-values
-   '((vc-default-patch-addressee . "bug-gnu-emacs@gnu.org")
-     (etags-regen-ignores "test/manual/etags/")
-     (etags-regen-regexp-alist
-      (("c" "objc") "/[ \11]*DEFVAR_[A-Z_ \11(]+\"\\([^\"]+\\)\"/\\1/"
-       "/[ \11]*DEFVAR_[A-Z_ \11(]+\"[^\"]+\",[ \11]\\([A-Za-z0-9_]+\\)/\\1/"))))
- '(vterm-keymap-exceptions
-   '("C-c" "C-x" "C-u" "C-g" "C-h" "C-l" "M-x" "M-o" "C-y" "M-y" "M-h"
-     "M-j" "M-k" "M-l")))
+   '(all-the-icons bui company-box consult-eglot consult-project-extra
+		   corfu diminish doom-modeline doom-themes
+		   dtrt-indent eat eldoc-box elixir-ts-mode
+		   embark-consult evil-collection
+		   evil-textobj-tree-sitter exec-path-from-shell
+		   eyebrowse flycheck general git-gutter-fringe gptel
+		   graphviz-dot-mode magit marginalia mini-frame
+		   mlscroll nerd-icons-corfu obsidian orderless
+		   pdf-tools plz pyvenv smooth-scrolling
+		   spacious-padding spinner sticky-scroll
+		   sticky-scroll-mode treemacs treesit-auto
+		   ultra-scroll vertico vterm zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t :background "#1a1b26")))
- '(header-line ((t :box (:line-width 4 :color "#161620" :style nil))))
- '(header-line-highlight ((t :box (:color "#a9b1d6"))))
- '(keycast-key ((t)))
- '(line-number ((t :background "#1a1b26")))
- '(mode-line ((t :box (:line-width 6 :color "#161620" :style nil))))
- '(mode-line-active ((t :box (:line-width 6 :color "#161620" :style nil))))
- '(mode-line-highlight ((t :box (:color "#a9b1d6"))))
- '(mode-line-inactive ((t :box (:line-width 6 :color "#171822" :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "#1a1b26" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#13141c" :style nil))))
- '(tab-line-tab ((t)))
- '(tab-line-tab-active ((t)))
- '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "#1a1b26" :foreground "#1a1b26")))
- '(window-divider ((t (:background "#1a1b26" :foreground "#1a1b26"))))
- '(window-divider-first-pixel ((t (:background "#1a1b26" :foreground "#1a1b26"))))
- '(window-divider-last-pixel ((t (:background "#1a1b26" :foreground "#1a1b26")))))
+ '(markdown-code-face ((t (:background "#292c33"))))
+ '(org-block ((t (:background "#292c33"))))
+ '(org-block-begin-line ((t (:background "#292c33"))))
+ '(org-block-end-line ((t (:background "#292c33"))))
+ '(org-code ((t (:background "#292c33")))))
--- init.el
+++ init.el
@@ -10,13 +10,26 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+(tab-bar-mode 1)
 
-; Smooth scroll
-(pixel-scroll-precision-mode 1)
+;; vsync fix
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
-; Disable annoying sounds
+; Turn on some things I like.
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;(add-hook 'prog-mode-hook 'hl-line-mode)
+
+; fix silly defaults
+(setopt sentence-end-double-space nil)
+(blink-cursor-mode -1)
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
+
+; turn off middle-click paste
+(global-unset-key [mouse-2])
+(global-unset-key [mouse-3])
+(global-set-key [mouse-3] 'eglot-code-actions-at-mouse)
+(global-set-key [down-mouse-3] 'mouse-drag-drag)
 
 ; Performance tweaks
 (setq gc-cons-threshold 100000000)
@@ -27,7 +40,15 @@
 (prefer-coding-system 'utf-8)
 
 ;; Font
-(set-frame-font (font-spec :family "JetBrainsMono Nerd Font" :size 15) nil t)
+;(set-frame-font (font-spec :family "JetBrainsMono Nerd Font" :size 15) nil t)
+(set-frame-font (font-spec :family "Iosevka Nerd Font" :size 17) nil t)
+
+; Eldoc tweaks
+;(setq eldoc-echo-area-display-truncation-message nil)
+;(setq eldoc-echo-area-use-multiline-p nil)
+;(setq eldoc-display-functions '(eldoc-display-in-echo-area eldoc-display-in-buffer))
+(setq eldoc-display-functions '(eldoc-display-in-buffer))
+(setq eldoc-idle-delay 0.1)
 
 ;; Global keybinds
 (global-set-key (kbd "M-l") 'evil-window-right)
@@ -54,30 +75,39 @@
 ;; Theme & Appearance
 (use-package doom-themes
   :init
-  (load-theme 'doom-tokyo-night t))
+  ;(setq doom-ir-black-brighter-comments 't)
+  ;(load-theme 'doom-monokai-spectrum)
+  (load-theme 'doom-Iosvkem)
+  (custom-set-faces
+   ;; Set a more legible background color for code blocks
+   '(markdown-code-face ((t (:background "#292c33"))))
+   '(org-code ((t (:background "#292c33"))))
+   '(org-block ((t (:background "#292c33"))))
+   '(org-block-begin-line ((t (:background "#292c33"))))
+   '(org-block-end-line ((t (:background "#292c33"))))
+   )
+  )
 
+;; Fast smooth-scrolling
+(use-package ultra-scroll
+  :vc (:url "https://github.com/jdtsmith/ultra-scroll"
+       :rev :newest)
+  :init
+  (setq scroll-conservatively 101 ; important!
+        scroll-margin 0)
+  :config
+  (ultra-scroll-mode 1))
 
 (use-package all-the-icons :defer)
 
-;; Quality-of-life improvements
-(use-package smooth-scrolling
-  :custom
-  (smooth-scrolling-mode 1)
-  (smooth-scroll-margin 6))
-
 ;; Env vars
-(use-package exec-path-from-shell
-  :ensure t
+(use-package exec-path-from-shell :ensure
   :init
   (exec-path-from-shell-copy-env "PATH")
   )
 
-;; Fix indentation
-(use-package dtrt-indent :ensure)
-
 ;; Add padding to make everything look more comfy
-(use-package spacious-padding
-  :ensure
+(use-package spacious-padding :ensure
   :init
   (spacious-padding-mode)
   )
@@ -95,7 +125,7 @@
   (evil-vsplit-window-right t)
   (evil-split-window-below t)
   (evil-shift-round nil)
-  (evil-want-C-u-scroll t)
+  (evil-want-C-u-scroll nil)
   (evil-want-keybinding nil)
   :config
   (evil-mode 1)
@@ -104,6 +134,11 @@
   (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
   (define-key evil-normal-state-map (kbd "g ]") 'xref-go-forward)
   (define-key evil-normal-state-map (kbd "g [") 'xref-go-back)
+  (define-key evil-normal-state-map (kbd "K") 'eldoc-box-help-at-point)
+  (define-key evil-normal-state-map [mouse-2] nil)
+  (define-key evil-insert-state-map [mouse-2] nil)
+  (define-key evil-visual-state-map [mouse-2] nil)
+  (define-key evil-motion-state-map [mouse-2] nil)
 
   ;; Add some quick fwd / bad keys
   (enable-smooth-scroll-for-function evil-next-visual-line)
@@ -116,18 +151,35 @@
   :after evil
   :diminish (evil-collection-unimpaired-mode)
   :init
-  (evil-collection-init))
+  ; prevent it from stealing K
+  (add-hook 'evil-collection-setup-hook
+            (lambda (mode keymaps)
+              (when (eq mode 'eglot)
+                (evil-collection-define-key 'normal 'eglot-mode-map
+                  "K" nil))))
+  (evil-collection-init)
+  )
 
 ;; Git
 (use-package magit :defer)
 
 (use-package git-gutter
-  :defer
+  :ensure
   :diminish
-  :init
-  (global-git-gutter-mode +1)
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.1)
   :custom
   (git-gutter:ask-p nil))
+
+(use-package git-gutter-fringe
+  :ensure
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+  (setq-default left-fringe-width 10)
+  )
 
 ;; Completion
 (use-package company
@@ -138,9 +190,24 @@
   :bind (
 	 ("C-<return>" . company-manual-begin))
   :custom
-  (company-idle-delay nil)
+  (company-idle-delay 0.25)
   (company-tooltip-align-annotations 't)
   (company-minimum-prefix-length 1))
+
+;; someday maybe I'll use corfu instead of company
+;; (use-package corfu
+;;   :ensure
+;;   :config
+;;   (global-corfu-mode)
+;;   (setq corfu-auto t
+;; 	corfu-quit-no-match 'separator)
+;;   )
+
+;; (use-package nerd-icons-corfu
+;;   :ensure
+;;   :config
+;;   (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)
+;;   )
 
 ;; Which Key
 (use-package which-key
@@ -165,9 +232,8 @@
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
-  (treesit-auto-add-to-auto-mode-alist 'all)
   :config
-  (treesit-auto-add-to-auto-mode-alist)
+  (treesit-auto-add-to-auto-mode-alist 'all)
   (delete 'janet treesit-auto-langs) ;; fails to compile for some reason - yeet
   (global-treesit-auto-mode))
 
@@ -176,6 +242,7 @@
   :hook (prog-mode . eglot-ensure)
   :init
   (setq eglot-stay-out-of '(flymake))
+  :config
   :bind (:map
 	 eglot-mode-map
 	 ("C-c c a" . eglot-code-actions)
@@ -196,7 +263,71 @@
 	      ("C-c ! p" . flycheck-goto-prev-error)
 	      ("C-c ! l" . flycheck-show-buffer-diagnostics)))
 
-(use-package eldoc-box :defer)
+(use-package eldoc-box
+  :defer
+  :config
+  (setq eldoc-box-clear-with-C-g 't)
+  ;(set-face-attribute 'eldoc-box-border nil :background "gray30")
+
+  ;; Fix eldoc-box + spacious-padding-mode
+  ;; Function to darken a color slightly
+  (defun my-darken-color (color)
+    "Return a slightly darker version of COLOR (a hex string like \"#RRGGBB\")."
+    (let* ((rgb (color-name-to-rgb color)) ; Convert to (R G B) list, 0.0-1.0
+           (r (max 0 (- (nth 0 rgb) 0.04))) ; Reduce each by 0.04, clamp at 0
+           (g (max 0 (- (nth 1 rgb) 0.04)))
+           (b (max 0 (- (nth 2 rgb) 0.04))))
+      (format "#%02x%02x%02x"
+              (floor (* r 255))  ; Convert back to 0-255 range
+              (floor (* g 255))
+              (floor (* b 255)))))
+  
+  ;; Function to update eldoc-box-frame-parameters and faces
+  (defun my-update-eldoc-box-padding (&rest _)
+    "Update `eldoc-box-frame-parameters`, `eldoc-box-border`, and `eldoc-box-body`.
+Ignore any arguments (for theme hook compatibility)."
+    (let ((params (copy-sequence eldoc-box-frame-parameters))
+          (default-bg (face-background 'default nil t)))
+      (if spacious-padding-mode
+          (let ((darker-bg (my-darken-color default-bg))) ; Darken the default background
+            ;; Update frame parameters
+            (setq params (assq-delete-all 'internal-border-width params))
+            (setq params (assq-delete-all 'left-fringe params))
+            (setq params (assq-delete-all 'right-fringe params))
+            (setq params (append params
+				 `((internal-border-width . ,(spacious-padding--get-internal-border-width))
+                                   (left-fringe . ,(or (spacious-padding--get-left-fringe-width)
+                                                       (spacious-padding--get-fringe-width)))
+                                   (right-fringe . ,(or (spacious-padding--get-right-fringe-width)
+							(spacious-padding--get-fringe-width))))))
+            ;; Set border and body faces to the darker background
+            (set-face-attribute 'eldoc-box-border nil :background darker-bg)
+            (set-face-attribute 'eldoc-box-body nil :background darker-bg))
+	;; Reset to original values when mode is off
+	(setq params (assq-delete-all 'internal-border-width params))
+	(setq params (assq-delete-all 'left-fringe params))
+	(setq params (assq-delete-all 'right-fringe params))
+	(setq params (append params '((internal-border-width . 1)
+                                      (left-fringe . 3)
+                                      (right-fringe . 3))))
+	(set-face-attribute 'eldoc-box-border nil
+                            :background (if (eq frame-background-mode 'dark) "white" "black"))
+	(set-face-attribute 'eldoc-box-body nil :background nil)) ; Reset to default
+      (setq eldoc-box-frame-parameters params)
+      ;; Reset existing frame if it exists
+      (when (and eldoc-box--frame (frame-live-p eldoc-box--frame))
+	(delete-frame eldoc-box--frame)
+	(setq eldoc-box--frame nil))))
+  
+  ;; Hook into spacious-padding-mode toggle
+  (add-hook 'spacious-padding-mode-hook #'my-update-eldoc-box-padding)
+  
+  ;; Hook into theme changes
+  (add-hook 'enable-theme-functions #'my-update-eldoc-box-padding)
+  
+  ;; Run once to apply current state
+  (my-update-eldoc-box-padding)
+  )
 
 ;; General keymap
 (use-package general
@@ -248,7 +379,6 @@
    "bi"  '(ibuffer :wk "ibuffer")
    "bn"  '(next-buffer :wk "next buffer")
    "bp"  '(previous-buffer :wk "previous buffer")
-   "TAB" '(switch-to-prev-buffer :wk "previous buffer")
    ;; Window
    "w"   '(:ignore t :wk "window")
    "w/"  '(split-window-right :wk "split right")
@@ -265,37 +395,27 @@
    "wo"  '(delete-other-windows :wk "delete other windows")
    ;; Git
    "g"   '(:ignore t :wk "git")
-   "gg"  '(magit-status :wk "status")
+   "gg"  '(magit-status :wk "magit status")
    "gS"  '(magit-stage :wk "stage file")
    "gU"  '(magit-unstage :wk "unstage file")
    "gc"  '(magit-commit :wk "commit")
-   "gd"  '(magit-diff :wk "diff")
+   "gd"  '(magit-file-dispatch :wk "dispatch")
    "gl"  '(magit-log :wk "log")
    "gb"  '(magit-blame :wk "blame")
-   "gp"  '(magit-push-to-remote :wk "push")
-   "gP"  '(magit-pull :wk "pull")
-   "gf"  '(magit-fetch :wk "fetch")
-   "gj"  '(git-gutter:next-hunk :wk "next hunk")
-   "gk"  '(git-gutter:previous-hunk :wk "previous hunk")
+   "gn"  '(git-gutter:next-hunk :wk "next hunk")
+   "gp"  '(git-gutter:previous-hunk :wk "previous hunk")
    "gs"  '(git-gutter:stage-hunk :wk "stage hunk")
-   "gr"  '(git-gutter:stage-hunk :wk "revert hunk")
-   "gm"  '(git-gutter:stage-hunk :wk "mark hunk")
+   "gr"  '(git-gutter:revert-hunk :wk "revert hunk")
+   "gh"  '(git-gutter:popup-hunk :wk "diff hunk")
    ;; Avy
    "l"    '(evil-avy-goto-line :wk "goto-line")
    "c"    '(evil-avy-goto-char-timer :wk "goto-char")
-   "."    '(evil-avy-goto-word-0 :wk "goto-word")
-   ;; Eyebrowse
-   "0"    '(eyebrowse-switch-to-window-config-0 :wk "workspace 0")
-   "1"    '(eyebrowse-switch-to-window-config-1 :wk "workspace 1")
-   "2"    '(eyebrowse-switch-to-window-config-2 :wk "workspace 2")
-   "3"    '(eyebrowse-switch-to-window-config-3 :wk "workspace 3")
-   "4"    '(eyebrowse-switch-to-window-config-4 :wk "workspace 4")
-   "5"    '(eyebrowse-switch-to-window-config-5 :wk "workspace 5")
    ;; Ace
    "a"    '(ace-select-window t :wk "ace")
    ;; Help
    "h"    '(:ignore t :wk "help and errors")
    "hh"   '(eldoc-box-help-at-point :wk "eldoc box help at point")
+   "he"   '(eldoc :wk "eldoc")
    "hl"   '(display-local-help :wk "display-local-help")
    "ho"   '(describe-package :wk "describe-package")
    "hf"   '(describe-function :wk "describe-function")
@@ -304,12 +424,6 @@
    "hm"   '(describe-mode :wk "describe-mode")
    "hn"   '(flymake-goto-next-error :wk "next error")
    "hp"   '(flymake-goto-prev-error :wk "prev error")
-   ;; Obsidian
-   "o"    '(:ignore t :wk "obsidian")
-   "od"   '(obsidian-daily-note :wk "daily note")
-   "oc"   '(obsidian-capture :wk "capture")
-   "os"   '(obsidian-search :wk "search")
-   "ot"   '(obsidian-tag-find :wk "find tag")
    ;; Search
    "/"    '(:ignore t :wk "consult")
    "//"   '(consult-line :wk "line")
@@ -318,9 +432,17 @@
    "/o"   '(consult-outline :wk "outline")
    "/b"   '(consult-buffer :wk "buffer")
    "/t"   '(consult-theme :wk "theme")
+   "/l"   '(consult-focus-lines :wk "focus lines")
+   ;; gptel
+   "."    '(:ignore t :wk "gptel")
+   ".."   '(gptel-menu :wk "gptel-menu")
+   ".a"   '(gptel-add :wk "gptel-add")
+   ".c"   '(gptel :wk "chat")
+   ".f"   '(gptel-context-add-file :wk "add/remove file")
+   ".x"   '(gptel-abort :wk "abort")
    ;; Other
    "i"   '((lambda () (interactive)(find-file "~/.config/emacs/init.el")) :wk "edit init.el")
-   "y"   '(vterm :wk "open terminal")
+   "y"   '(eat :wk "open terminal")
    "f"   '(find-file :wk "find file")
    "t"   '(treemacs :wk "treemacs")
    "'"   '(comment-or-uncomment-region :wk "toggle comment")
@@ -328,59 +450,30 @@
 )
 
 ;; Navigation
-(use-package avy
-  :ensure
-  :defer
-  :diminish
+(use-package avy :ensure :defer :diminish
   :custom
-  (avy-timeout-seconds 0.2)
-  )
+  (avy-timeout-seconds 0.15))
 
-(use-package ace-window
-  :ensure
-  :defer
-  :diminish
-  :custom
-  (ace-window-display-mode t))
-
-(use-package eyebrowse
-  :config
-  (eyebrowse-mode)
-  :custom
-  (eyebrowse-new-workspace t))
+(use-package ace-window :ensure :defer :diminish)
 
 ;; Term
-(use-package vterm
-  :defer)
+(use-package vterm :ensure :defer
+  :init
+  (add-hook 'vterm-mode-hook 'evil-insert-state)
+  )
+(use-package eat :ensure :defer
+  :init
+  (add-hook 'eat-mode-hook 'evil-insert-state)
+  )
 
-;; Obsidian
-(use-package obsidian
-  :ensure t
-  :demand t
+(use-package markdown-mode
+  :ensure
+  :defer
   :config
-  (obsidian-specify-path "~/Documents/Obsidian Vault")
-  (global-obsidian-mode t)
-  :custom
-  ;; This directory will be used for `obsidian-capture' if set.
-  (obsidian-inbox-directory "Random")
-  ;; Create missing files in inbox? - when clicking on a wiki link
-  ;; t: in inbox, nil: next to the file with the link
-  ;; default: t
-  ;(obsidian-wiki-link-create-file-in-inbox nil)
-  ;; The directory for daily notes (file name is YYYY-MM-DD.md)
-  (obsidian-daily-notes-directory "Daily")
-  ;; Directory of note templates, unset (nil) by default
-  ;(obsidian-templates-directory "Templates")
-  ;; Daily Note template name - requires a template directory. Default: Daily Note Template.md
-  ;(obsidian-daily-note-template "Daily Note Template.md")
-  :bind (:map obsidian-mode-map
-  ("C-c M-o" . obsidian-hydra/body)
-  ;; Replace C-c C-o with Obsidian.el's implementation. It's ok to use another key binding.
-  ("C-c C-o" . obsidian-follow-link-at-point)
-  ;; Jump to backlinks
-  ("C-c C-b" . obsidian-backlink-jump)
-  ;; If you prefer you can use `obsidian-insert-link'
-  ("C-c C-l" . obsidian-insert-wikilink)))
+  (setq markdown-fontify-code-blocks-natively t)
+  )
+
+(use-package pdf-tools :ensure :defer)
 
 (use-package doom-modeline
   :ensure t
@@ -405,7 +498,7 @@
   :config
   (marginalia-mode 1))
 
-;; out-of-order fuzzy matching / completion
+;; out-of-order fuzzy matching / complettion
 (use-package orderless
   :ensure
   :config
@@ -437,18 +530,64 @@
 (use-package embark-consult
   :ensure)
 
-;; Markdown customization
-(setq markdown-fontify-code-blocks-natively t)
+;; gptel
+(use-package gptel
+  :ensure
+  :defer
+  :config
+  (setq gptel-confirm-tool-calls 't)
+  (gptel-make-anthropic "Claude"
+    :stream t
+    :key (getenv "CLAUDE_API_KEY"))
+  
+  (setq gptel-model 'Qwen2.5-Coder-32B-Instruct-exl2-8bpw-8hb
+        gptel-backend (gptel-make-openai "local"
+			:host (getenv "LOCAL_API_URL")
+			:protocol "http"
+			:endpoint "/v1/chat/completions"
+			:stream t
+			:key (getenv "LOCAL_API_KEY")
+			:models '("Qwen2.5-Coder-32B-Instruct-exl2-8bpw-8hb")))
+  )
+
+(use-package mlscroll
+  :ensure t
+  :config
+  (mlscroll-mode 1))
+
+(use-package pyvenv :ensure :defer)
 
 ;;; Manually customized variables
 (setq scroll-preserve-screen-position 1) ; fix scrolling?
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 (setq create-lockfiles nil)  ; Temporary to make react dev server not puke...?
+(setq right-divider-width 3)
+
+
+;; Custom variable for Esc clearing (if not already defined)
+(defcustom eldoc-box-clear-with-esc nil
+  "If non-nil, clear the eldoc-box childframe when pressing Esc."
+  :type 'boolean
+  :group 'eldoc-box)
+
+;; Function to quit frame on Esc
+(defun my-eldoc-box-quit-on-esc (&rest _)
+  "Quit the eldoc-box frame if `eldoc-box-clear-with-esc' is non-nil."
+  (when eldoc-box-clear-with-esc
+    (eldoc-box-quit-frame)))
+
+;; Advise evil-force-normal-state instead of keyboard-escape-quit
+(advice-add 'evil-force-normal-state :before #'my-eldoc-box-quit-on-esc)
+
+;; Enable Esc clearing by default (optional)
+(setq eldoc-box-clear-with-esc t)
 
 ;; Load auto-customized vars & faces
 (load custom-file)
 
+(put 'dired-find-alterate-file 'disabled nil)
+(setq dired-kill-when-opening-new-dired-buffer t)
+
 (provide 'init)
 ;;; init.el ends here
-(put 'dired-find-alternate-file 'disabled nil)
